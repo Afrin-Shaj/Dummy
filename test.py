dd929b628c3cddb61a2782aa7204e4e698a56c65
Afrin <afrin.s.ihub@snsgroups.com>
2026-03-05 14:57:48 +0530

    feat: sec

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-API Test Database
+let storedUser = "admin";
+let storedPassword = "admin123";
+
+let inputUser = "admin";
+let inputPassword = "admin123";
+
+let login = false;
+
+if(inputUser == storedUser){
+
+   if(inputPassword == storedPassword){
+       login = true;
+   }
+
+}
+
+console.log("Login:", login);
+
+let command = "alert('security test')";
+
+let runCommand = command;
+
+eval(runCommand);
+
+let accessLevel = "admin";
+
+if(accessLevel == "admin"){
+   console.log("Full access granted");
+}
